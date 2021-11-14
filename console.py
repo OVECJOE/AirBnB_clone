55a2d244267593a43c78a7c7fb2d94d7d7c7c6d7
OVECJOE <ovecjoe123@gmail.com>
2021-11-14 18:11:06 +0100

    Testing some fixes...

--- console.py
+++ console.py
@@ -66,7 +66,8 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, argv):
         """Creates a new instance of BaseModel, saves it (to a JSON file)
         and prints the id"""
-        if (args := check_args(argv)) is not None:
+        args = check_args(argv)
+        if args:
             print(eval(args[0])().id)
             self.storage.save()
 
@@ -78,7 +79,7 @@ class HBNBCommand(cmd.Cmd):
             if len(args) != 2:
                 print("** instance id missing **")
             else:
-                key = f"{args[0]}.{args[1]}"
+                key = "{}.{}".format(args[0], args[1])
                 if key not in self.storage.all():
                     print("** no instance found **")
                 else:
