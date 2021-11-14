7e46efe3c21bd460f9ec96af4437208819256a0b
OVECJOE <ovecjoe123@gmail.com>
2021-11-14 19:35:53 +0100

    Testing some fixes...

--- console.py
+++ console.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """contains the entry point of the command interpreter"""
-import re
 import cmd
 from shlex import split
 
@@ -14,22 +13,32 @@ from models.state import State
 from models.review import Review
 
 
-def parse(arg):
-    curly_braces = re.search(r"\{(.*?)\}", arg)
-    brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_braces is None:
-        if brackets is None:
-            return [i.strip(",") for i in split(arg)]
-        else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
+def check_args(args):
+    """checks if args is valid
+
+    Args:
+        args (str): the string containing the arguments passed to a command
+
+    Returns:
+        Error message if args is None or not a valid class, else the arguments
+    """
+    arg_list = split(args)
+    CLASSES = [
+        "BaseModel",
+        "User",
+        "City",
+        "Place",
+        "State",
+        "Amenity",
+        "Review"
+    ]
+
+    if len(arg_list) == 0:
+        print("** class name missing **")
+    elif arg_list[0] not in CLASSES:
+        print("** class doesn't exist **")
     else:
-        lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
-        retl.append(curly_braces.group())
-        return retl
+        return arg_list
 
 
 class HBNBCommand(cmd.Cmd):
@@ -38,7 +47,7 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = "(hbnb) "
     storage = models.storage
-    __CLASSES = [
+    CLASSES = [
         "BaseModel",
         "User",
         "City",
@@ -64,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, argv):
         """Creates a new instance of BaseModel, saves it (to a JSON file)
         and prints the id"""
-        args = parse(argv)
+        args = check_args(argv)
         if args:
             print(eval(args[0])().id)
             self.storage.save()
@@ -72,7 +81,7 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, argv):
         """Prints the string representation of an instance based
         on the class name and id"""
-        args = parse(argv)
+        args = check_args(argv)
         if args:
             if len(args) != 2:
                 print("** instance id missing **")
@@ -86,12 +95,12 @@ class HBNBCommand(cmd.Cmd):
     def do_all(self, argv):
         """Prints all string representation of all instances based or not
         based on the class name"""
-        arg_list = parse(argv)
+        arg_list = split(argv)
         objects = self.storage.all().values()
         if not arg_list:
             print([str(obj) for obj in objects])
         else:
-            if arg_list[0] not in self.__CLASSES:
+            if arg_list[0] not in CLASSES:
                 print("** class doesn't exist **")
             else:
                 print([str(obj) for obj in objects
@@ -99,7 +108,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, argv):
         """Delete a class instance based on the name and given id."""
-        arg_list = parse(argv)
+        arg_list = check_args(argv)
         if arg_list:
             if len(arg_list) == 1:
                 print("** instance id missing **")
@@ -114,7 +123,7 @@ class HBNBCommand(cmd.Cmd):
     def do_update(self, argv):
         """Updates an instance based on the class name and id by adding or
         updating attribute and save it to the JSON file."""
-        arg_list = parse(argv)
+        arg_list = check_args(argv)
         if arg_list:
             if len(arg_list) == 1:
                 print("** instance id missing **")
