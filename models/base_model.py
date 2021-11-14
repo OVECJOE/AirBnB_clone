5b3095d840a48ea3a4bd65f267f208665eb91d42
OVECJOE <ovecjoe123@gmail.com>
2021-11-14 17:34:34 +0100

    Testing some fixes...

--- models/base_model.py
+++ models/base_model.py
@@ -18,25 +18,18 @@ class BaseModel:
         Initialize the BaseModel class
         """
 
-        # super().__init__()
-        # self.id = str(uuid4())
-        # self.created_at = self.updated_at = datetime.now()
-        # something doesn't add up here.
+        super().__init__()
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.now()
 
         if not kwargs:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
             models.storage.new(self)
 
-        else:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
-                elif k != "__class__":
-                    pass
-                else:
-                    setattr(self, k, v)
+        for k, v in kwargs.items():
+            if k == "created_at" or k == "updated_at":
+                v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+            if k != "__class__":
+                setattr(self, k, v)
 
     def __str__(self):
         """
@@ -51,7 +44,6 @@ class BaseModel:
         Updates 'self.updated_at' with the current datetime
         """
         self.updated_at = datetime.now()
-        model.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -65,19 +57,19 @@ class BaseModel:
         object
         """
 
-        # attr_set = {k: v for k, v in self.__dict__.items()
-        #             if not k.startswith('_')}
-        # attr_set["__class__"] = type(self).__name__
-        # attr_set["created_at"] = attr_set["created_at"].isoformat()
-        # attr_set["updated_at"] = attr_set["updated_at"].isoformat()
-        # return attr_set
+        attr_set = {k: v for k, v in self.__dict__.items()
+                    if not k.startswith('_')}
+        attr_set["__class__"] = type(self).__name__
+        attr_set["created_at"] = attr_set["created_at"].isoformat()
+        attr_set["updated_at"] = attr_set["updated_at"].isoformat()
+        return attr_set
 
         # -> instead of changing an argument at a time,
         # we can use an if statement to do it all at once
-        dict_1 = self.__dict__.copy()
-        dict_1["__class__"] = self.__class__.__name__
-        for k, v in self.__dict__.items():
-            if k in ["created_at", "updated_at"]:
-                v = self.__dict__[k].isoformat()
-                dict_1[k] = v
-        return dict_1
+        # dict_1 = self.__dict__.copy()
+        # dict_1["__class__"] = self.__class__.__name__
+        # for k, v in self.__dict__.items():
+        #     if k in ["created_at", "updated_at"]:
+        #         v = self.__dict__[k].isoformat()
+        #         dict_1[k] = v
+        # return dict_1
