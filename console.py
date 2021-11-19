fe2b82a559a809d95dc16d5ab3388126e1d66cc3
OVECJOE <ovecjoe123@gmail.com>
2021-11-19 16:43:31 +0100

    Updated console.py

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """contains the entry point of the command interpreter"""
 import cmd
+import re
 from shlex import split
 
 import models
@@ -24,6 +25,24 @@ CLASSES = [
 ]
 
 
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
+
+
 def check_args(args):
     """checks if args is valid
 
@@ -33,7 +52,7 @@ def check_args(args):
     Returns:
         Error message if args is None or not a valid class, else the arguments
     """
-    arg_list = split(args)
+    arg_list = parse(args)
 
     if len(arg_list) == 0:
         print("** class name missing **")
@@ -53,6 +72,30 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Command to executed when empty line + <ENTER> key"""
         pass
+
+    def default(self, arg):
+        """Default behaviour for cmd module when input is invalid"""
+        action_map = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update,
+            "create": self.do_create
+        }
+
+        match = re.search(r"\.", arg)
+        if match:
+            arg1 = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", arg1[1])
+            if match:
+                command = [arg1[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in action_map:
+                    call = "{} {}".format(arg1[0], command[1])
+                    return action_map[command[0]](call)
+
+        print("*** Unknown syntax: {}".format(arg))
+        return False
 
     def do_EOF(self, argv):
         """EOF signal to exit the program"""
@@ -138,6 +181,10 @@ class HBNBCommand(cmd.Cmd):
                     print("** no instance found **")
 
             self.storage.save()
+
+    def do_count(self, arg):
+        """Retrieve the number of instances of a class"""
+        pass
 
 
 if __name__ == "__main__":
